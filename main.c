9681f3979cf65b2bf6481ca4dcf5df1c65ff15dd
wupeicong <1097727091@qq.com>
2018-10-15 18:30:30 +0800

    改了点bug

--- main.c
+++ main.c
@@ -5,10 +5,17 @@
 #include"ctrler.h"
 #include"process.h"
 
+extern byte *shmaddr;
+
 int main(){
     shm_init();
     
-    create_process();
+    //create_process();
 
-    ele_main_loop();
+    //ele_main_loop();
+    shm_write(shmaddr,"goodday!",9);
+    byte buf[9];
+    shm_read(shmaddr,buf,9);
+    printf("%s\n",buf);
+
 }
